29ecd8f2e8fca69447328a4752f7d75bf3b6ae0b
Andreas Wild <codr7@protonmail.com>
2024-04-29 17:50:51 +0200

    Use namespace.

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System.Diagnostics;
 using Sharpl;
-using Sharpl.Operations;
+using Operations = Sharpl.Operations;
 
 var vm = new VM();
 var Int = new Type<int>("Int");
 var v = Value.Make(Int, 42);
-vm.Emit(Push.Make(v));
-vm.Emit(Stop.Make());
+vm.Emit(Operations.Push.Make(v));
+vm.Emit(Operations.Stop.Make());
 var stack = new Stack<Value>(32);
 vm.Eval(0, stack);
 Debug.Assert(stack.Peek() == v);
