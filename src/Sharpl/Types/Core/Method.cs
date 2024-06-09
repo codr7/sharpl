ae4124390ab62b35fe4f21dc3c315533ba03541b
Andreas Wild <codr7@protonmail.com>
2024-06-10 01:47:18 +0200

    Check arguments on emit.

--- src/Sharpl/Types/Core/Method.cs
+++ src/Sharpl/Types/Core/Method.cs
@@ -6,8 +6,15 @@ public class MethodType : Type<Method>
 
     public override void EmitCall(Loc loc, VM vm, Env env, Value target, Form.Queue args)
     {
+        var m = target.Cast(this);
         var arity = args.Count;
+
+        if (arity < m.Args.Length)
+        {
+            throw new EmitError(loc, $"Not enough arguments: {m}");
+        }
+
         args.Emit(vm, env);
-        vm.Emit(Ops.CallMethod.Make(loc, target.Cast(this), arity));
+        vm.Emit(Ops.CallMethod.Make(loc, m, arity));
     }
 }
