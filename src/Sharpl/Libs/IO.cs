c848a93ea50ec02d838986f2298b3d534436bf00
Andreas Wild <codr7@protonmail.com>
2024-06-27 22:35:37 +0200

    Todo.

--- src/Sharpl/Libs/IO.cs
+++ src/Sharpl/Libs/IO.cs
@@ -3,8 +3,6 @@ namespace Sharpl.Libs;
 using Sharpl.Iters.IO;
 using Sharpl.Types.IO;
 
-using System.Text;
-
 public class IO : Lib
 {
     public static readonly StreamReaderType StreamReader = new StreamReaderType("StreamReader");
@@ -17,21 +15,25 @@ public class IO : Lib
 
         BindMacro("do-read", ["path"], (loc, target, vm, args) =>
          {
-            if (args.Pop() is Form f) {                
-                var startPC = vm.EmitPC;
-                f.Emit(vm, args);
-                var reg = vm.AllocRegister();
-                vm.Emit(Ops.OpenStreamReader.Make(loc, 0, reg));
-                args.Emit(vm);
-                vm.Emit(Ops.Stop.Make());
-                var stack = new Stack(vm.Config.MaxStackSize);
-                
-                try {
-                    vm.Eval(startPC, stack);
-                } finally {
-                    vm.GetRegister(0, reg).Cast(IO.StreamReader).Close();
-                }
-            }
+             if (args.Pop() is Form f)
+             {
+                 var startPC = vm.EmitPC;
+                 f.Emit(vm, args);
+                 var reg = vm.AllocRegister();
+                 vm.Emit(Ops.OpenStreamReader.Make(loc, 0, reg));
+                 args.Emit(vm);
+                 vm.Emit(Ops.Stop.Make());
+                 var stack = new Stack(vm.Config.MaxStackSize);
+
+                 try
+                 {
+                     vm.Eval(startPC, stack);
+                 }
+                 finally
+                 {
+                     vm.GetRegister(0, reg).Cast(IO.StreamReader).Close();
+                 }
+             }
          });
 
         BindMethod("lines", ["in"], (loc, target, vm, stack, arity) =>
