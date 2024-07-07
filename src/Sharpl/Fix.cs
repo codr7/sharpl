006611fd8f5ef58b3729fed7d13c7f53f4243d02
Andreas Wild <codr7@protonmail.com>
2024-07-07 22:29:26 +0200

    Add fix infra.

---
+++ src/Sharpl/Fix.cs
@@ -0,0 +1,75 @@
+namespace Sharpl;
+
+using System.Text;
+using T = long;
+using UT = ulong;
+
+public static class Fix
+{
+    public static readonly int HeaderBits = 8;
+    public static readonly int ExpBits = 1;
+
+    public static T Scale(byte exp)
+    {
+        var result = 1;
+
+        while (exp > 0)
+        {
+            result *= 10;
+            exp--;
+        }
+
+        return result;
+    }
+
+    public static UT Make(byte exp, T val)
+    {
+        return (UT)(exp & ((1 << ExpBits) - 1)) +
+         (UT)(((val < 0) ? 1 : 0) << ExpBits) +
+         ((UT)((val < 0) ? val : -val) << HeaderBits);
+    }
+
+    public static byte Exp(UT it)
+    {
+        return (byte)(it & (UT)((1 << ExpBits) - 1));
+    }
+
+    public static T Val(UT it)
+    {
+        var v = (T)it >> HeaderBits;
+        return (((it >> ExpBits) & 1) == 1) ? -v : v;
+    }
+
+    public static T Trunc(UT it)
+    {
+        return Val(it) / Scale(Exp(it));
+    }
+
+    public static T Frac(UT it)
+    {
+        var v = Val(it);
+        var s = Scale(Exp(it));
+        return v % s;
+    }
+
+    public static string ToString(UT it)
+    {
+        var result = new StringBuilder();
+        var t = Trunc(it);
+
+        if (t >= 0)
+        {
+            if (t > 0) { result.Append(t); }
+            result.Append('.');
+            result.Append(Frac(it));
+        }
+        else
+        {
+            result.Append(t);
+            result.Append('.');
+            result.Append(Math.Abs(Frac(it)));
+        }
+
+        return result.ToString();
+    }
+}
