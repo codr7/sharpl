37c17d38f950e49eb9ac9f577a54677b028393f7
Andreas Wild <codr7@protonmail.com>
2024-07-25 03:06:58 +0200

    Use List.

--- src/Sharpl/ArrayStack.cs
+++ src/Sharpl/ArrayStack.cs
@@ -5,33 +5,32 @@ namespace Sharpl;
 
 public class ArrayStack<T> : IEnumerable<T>, IList<T>
 {
-    private readonly T[] items;
-    private int count = 0;
-
+    private readonly List<T> items = new List<T>();
+ 
     public T this[int i]
     {
         get => Get(i);
         set => Set(i, value);
     }
 
-    public int Count { get { return count; } }
+    public int Count { get { return items.Count; } }
 
     public ArrayStack(int capacity)
     {
-        items = new T[capacity];
+        items.EnsureCapacity(capacity);
     }
 
     public void Clear()
     {
-        count = 0;
+        items.Clear();
     }
 
     public void Drop(int n)
     {
-        count -= n;
+        items.RemoveRange(items.Count - n, n);
     }
 
-    public bool Empty { get { return count == 0; } }
+    public bool Empty { get { return items.Count == 0; } }
 
     public bool IsReadOnly => throw new NotImplementedException();
 
@@ -42,24 +41,25 @@ public class ArrayStack<T> : IEnumerable<T>, IList<T>
 
     public T Peek(int offset = 0)
     {   
-        return items[count - 1 - offset];
+        return items[items.Count - 1 - offset];
     }
 
     public T Pop()
     {
-        count--;
-        return items[count];
+        var i = items.Count - 1;
+        var v = items[i];
+        items.RemoveAt(i);
+        return v;
     }
 
     public void Push(T it)
     {
-        items[count] = it;
-        count++;
+        items.Add(it);
     }
 
     public void Reverse(int n)
     {
-        Array.Reverse(items, Count - n, n);
+        items.Reverse(Count - n, n);
     }
 
     public void Set(int i, T value)
@@ -77,7 +77,7 @@ public class ArrayStack<T> : IEnumerable<T>, IList<T>
         var res = new StringBuilder();
         res.Append('[');
 
-        for (var i = 0; i < count; i++)
+        for (var i = 0; i < items.Count; i++)
         {
             if (i > 0)
             {
@@ -98,32 +98,32 @@ public class ArrayStack<T> : IEnumerable<T>, IList<T>
 
     public void Trunc(int n)
     {
-        count = n;
+        items.RemoveRange(n, items.Count - n);
     }
 
     public IEnumerator<T> GetEnumerator()
     {
-        return items[0..count].AsEnumerable().GetEnumerator();
+        return items.GetEnumerator();
     }
 
     IEnumerator IEnumerable.GetEnumerator()
     {
-        return items[0..count].GetEnumerator();
+        return items.GetEnumerator();
     }
 
-    public int IndexOf(T item)
+    public int IndexOf(T it)
     {
-        return Array.IndexOf(items, item);
+        return items.IndexOf(it);
     }
 
-    public void Insert(int index, T item)
+    public void Insert(int i, T it)
     {
-        throw new NotImplementedException();
+        items.Insert(i, it);
     }
 
-    public void RemoveAt(int index)
+    public void RemoveAt(int i)
     {
-        throw new NotImplementedException();
+        items.RemoveAt(i);
     }
 
     public void Add(T item)
@@ -131,24 +131,18 @@ public class ArrayStack<T> : IEnumerable<T>, IList<T>
         Push(item);
     }
 
-    public bool Contains(T item)
+    public bool Contains(T it)
     {
-        return Array.IndexOf(items, item) > -1;
+        return items.Contains(it);
     }
 
     public void CopyTo(T[] array, int arrayIndex)
     {
-        Array.Copy(items[arrayIndex..], array, count);
+        items.CopyTo(array, arrayIndex);
     }
 
     public bool Remove(T item)
     {
-        if (count > 0)
-        {
-            Pop();
-            return true;
-        }
-
-        return false;
+        return items.Remove(item);
     }
 }
