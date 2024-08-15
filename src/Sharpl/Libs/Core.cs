bf2f590dfd7f1418ad74a1a8822028969eab0df3
Andreas Wild <codr7@protonmail.com>
2024-08-15 13:44:07 +0200

    Cleaning.

--- src/Sharpl/Libs/Core.cs
+++ src/Sharpl/Libs/Core.cs
@@ -1,9 +1,8 @@
-namespace Sharpl.Libs;
-
 using Sharpl.Types.Core;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+
+namespace Sharpl.Libs;
 
 public class Core : Lib
 {
@@ -56,10 +55,7 @@ public class Core : Lib
             {
                 fas = af.Items.Select(f =>
                 {
-                    if (vararg)
-                    {
-                        throw new EmitError(f.Loc, "Vararg must be final param.");
-                    }
+                    if (vararg) { throw new EmitError(f.Loc, "Vararg must be final param."); }
 
                     if (f is Forms.Splat s)
                     {
@@ -77,10 +73,7 @@ public class Core : Lib
                     throw new EmitError(f.Loc, $"Invalid method arg: {f}");
                 }).ToArray();
             }
-            else
-            {
-                throw new EmitError(loc, "Invalid method args");
-            }
+            else { throw new EmitError(loc, "Invalid method args"); }
 
             var m = new UserMethod(loc, vm, name, ids.ToArray(), fas, vararg);
             if (ids.Count > 0) { vm.Emit(Ops.PrepareClosure.Make(m)); }
@@ -119,9 +112,7 @@ public class Core : Lib
         Bind("T", Value.T);
 
         BindMacro("^", [], (loc, target, vm, args) =>
-         {
-             DefineMethod(loc, vm, args, UserMethod, Ops.ExitMethod.Make());
-         });
+            DefineMethod(loc, vm, args, UserMethod, Ops.ExitMethod.Make()));
 
         BindMethod("=", ["x"], (loc, target, vm, stack, arity) =>
         {
@@ -148,20 +139,12 @@ public class Core : Lib
             var lv = stack.Pop();
             arity--;
             var res = true;
-
-            if (lv.Type is not ComparableTrait t)
-            {
-                throw new EvalError(loc, $"Not comparable: {lv}");
-            }
+            if (lv.Type is not ComparableTrait t) { throw new EvalError(loc, $"Not comparable: {lv}"); }
 
             while (arity > 0)
             {
                 var rv = stack.Pop();
-
-                if (rv.Type != t)
-                {
-                    throw new EvalError(loc, $"Type mismatch: {lv} {rv}");
-                }
+                if (rv.Type != t) { throw new EvalError(loc, $"Type mismatch: {lv} {rv}"); }
 
                 if (t.Compare(lv, rv) != Order.GT)
                 {
@@ -181,20 +164,12 @@ public class Core : Lib
             arity--;
             var res = true;
             var t = lv.Type as ComparableTrait;
-
-            if (t is null)
-            {
-                throw new EvalError(loc, $"Not comparable: {lv}");
-            }
+            if (t is null) { throw new EvalError(loc, $"Not comparable: {lv}"); }
 
             while (arity > 0)
             {
                 var rv = stack.Pop();
-
-                if (rv.Type != t)
-                {
-                    throw new EvalError(loc, $"Type mismatch: {lv} {rv}");
-                }
+                if (rv.Type != t) { throw new EvalError(loc, $"Type mismatch: {lv} {rv}"); }
 
                 if (t.Compare(lv, rv) != Order.LT)
                 {
@@ -211,58 +186,27 @@ public class Core : Lib
 
         BindMethod("+", [], (loc, target, vm, stack, arity) =>
         {
-            var v = stack.Peek();
-
-            if (v.Type is NumericTrait nt)
-            {
-                nt.Add(loc, vm, stack, arity);
-            }
-            else
-            {
-                throw new EvalError(loc, $"Expected numeric value: {v}");
-            }
+            if (stack.Peek().Type is NumericTrait nt) { nt.Add(loc, vm, stack, arity); }
+            else { throw new EvalError(loc, $"Expected numeric value: {v}"); }
         });
 
         BindMethod("-", [], (loc, target, vm, stack, arity) =>
         {
-            var v = stack.Peek();
-
-            if (v.Type is NumericTrait nt)
-            {
-                nt.Subtract(loc, vm, stack, arity);
-            }
-            else
-            {
-                throw new EvalError(loc, $"Expected numeric value: {v}");
-            }
+            if (stack.Peek().Type is NumericTrait nt) { nt.Subtract(loc, vm, stack, arity); }
+            else { throw new EvalError(loc, $"Expected numeric value: {v}"); }
         });
 
         BindMethod("*", ["x", "y"], (loc, target, vm, stack, arity) =>
          {
-             var v = stack.Peek();
-
-             if (v.Type is NumericTrait nt)
-             {
-                 nt.Multiply(loc, vm, stack, arity);
-             }
-             else
-             {
-                 throw new EvalError(loc, $"Expected numeric value: {v}");
-             }
+             if (stack.Peek().Type is NumericTrait nt)
+             { nt.Multiply(loc, vm, stack, arity); }
+             else { throw new EvalError(loc, $"Expected numeric value: {v}"); }
          });
 
         BindMethod("/", ["x", "y"], (loc, target, vm, stack, arity) =>
         {
-            var v = stack.Peek();
-
-            if (v.Type is NumericTrait nt)
-            {
-                nt.Divide(loc, vm, stack, arity);
-            }
-            else
-            {
-                throw new EvalError(loc, $"Expected numeric value: {v}");
-            }
+            if (stack.Peek().Type is NumericTrait nt) { nt.Divide(loc, vm, stack, arity); }
+            else { throw new EvalError(loc, $"Expected numeric value: {v}"); }
         });
 
         BindMacro("bench", ["n"], (loc, target, vm, args) =>
@@ -273,10 +217,7 @@ public class Core : Lib
                  args.Emit(vm, new Form.Queue());
                  vm.Emit(Ops.Stop.Make());
              }
-             else
-             {
-                 throw new EmitError(loc, "Missing repetitions");
-             }
+             else { throw new EmitError(loc, "Missing repetitions"); }
          });
 
         BindMacro("check", ["x"], (loc, target, vm, args) =>
@@ -290,17 +231,10 @@ public class Core : Lib
 
                  vm.DoEnv(new Env(vm.Env, args.CollectIds()), () =>
                  {
-
                      while (true)
                      {
-                         if (args.TryPop() is Form bf)
-                         {
-                             bf.Emit(vm, args);
-                         }
-                         else
-                         {
-                             break;
-                         }
+                         if (args.TryPop() is Form bf) { bf.Emit(vm, args); }
+                         else { break; }
                      }
 
                  });
@@ -326,15 +260,9 @@ public class Core : Lib
                         vm.Emit(Ops.Decrement.Make(b.FrameOffset, b.Index));
                     }
                 }
-                else
-                {
-                    throw new EmitError(id.Loc, "Invalid target");
-                }
+                else { throw new EmitError(id.Loc, "Invalid target"); }
             }
-            else
-            {
-                throw new EmitError(loc, "Invalid target");
-            }
+            else { throw new EmitError(loc, "Invalid target"); }
         });
 
         BindMacro("demit", [], (loc, target, vm, args) =>
@@ -352,12 +280,7 @@ public class Core : Lib
         BindMacro("do", [], (loc, target, vm, args) =>
         {
             vm.Emit(Ops.BeginFrame.Make(vm.NextRegisterIndex));
-
-            vm.DoEnv(new Env(vm.Env, args.CollectIds()), () =>
-            {
-                args.Emit(vm, new Form.Queue());
-            });
-
+            vm.DoEnv(new Env(vm.Env, args.CollectIds()), () => args.Emit(vm, new Form.Queue()));
             vm.Emit(Ops.EndFrame.Make());
         });
 
@@ -397,15 +320,8 @@ public class Core : Lib
 
             vm.DoEnv(new Env(vm.Env, args.CollectIds()), () =>
             {
-
-                if (args.TryPop() is Form f)
-                {
-                    f.Emit(vm, args);
-                }
-                else
-                {
-                    throw new EmitError(loc, "Missing condition");
-                }
+                if (args.TryPop() is Form f) { f.Emit(vm, args); }
+                else { throw new EmitError(loc, "Missing condition"); }
 
                 var skip = new Label();
                 vm.Emit(Ops.Branch.Make(skip));
@@ -421,24 +337,11 @@ public class Core : Lib
 
              vm.DoEnv(new Env(vm.Env, args.CollectIds()), () =>
              {
-
-                 if (args.TryPop() is Form cf)
-                 {
-                     cf.Emit(vm, args);
-                 }
-                 else
-                 {
-                     throw new EmitError(loc, "Missing condition");
-                 }
-
+                 if (args.TryPop() is Form cf) { cf.Emit(vm, args); }
+                 else { throw new EmitError(loc, "Missing condition"); }
                  var skipElse = new Label();
                  vm.Emit(Ops.Branch.Make(skipElse));
-
-                 if (args.TryPop() is Form tf)
-                 {
-                     tf.Emit(vm, args);
-                 }
-
+                 if (args.TryPop() is Form tf) { tf.Emit(vm, args); }
                  var skipEnd = new Label();
                  vm.Emit(Ops.Goto.Make(skipEnd));
                  skipElse.PC = vm.EmitPC;
@@ -474,14 +377,8 @@ public class Core : Lib
         {
             var v = stack.Pop();
 
-            if (v.Type is SeqTrait st)
-            {
-                stack.Push(Core.Int, st.Length(v));
-            }
-            else
-            {
-                throw new EvalError(loc, $"Expected sequence: {v}");
-            }
+            if (v.Type is SeqTrait st) { stack.Push(Core.Int, st.Length(v)); }
+            else { throw new EvalError(loc, $"Expected sequence: {v}"); }
         });
 
         BindMacro("let", ["bindings"], (loc, target, vm, args) =>
@@ -494,10 +391,7 @@ public class Core : Lib
 
                 for (var i = 0; i < bs.Length; i += 2)
                 {
-                    if (bs[i] is Forms.Id f)
-                    {
-                        ids.Remove(f.Name);
-                    }
+                    if (bs[i] is Forms.Id f) { ids.Remove(f.Name); }
                 }
 
                 vm.Emit(Ops.BeginFrame.Make(vm.NextRegisterIndex));
@@ -530,22 +424,13 @@ public class Core : Lib
                                 vm.Env[idf.Name] = Value.Make(Core.Binding, new Register(0, r));
                             }
                         }
-                        else
-                        {
-                            throw new EmitError(bf.Loc, $"Invalid method arg: {bf}");
-                        }
+                        else { throw new EmitError(bf.Loc, $"Invalid method arg: {bf}"); }
                     }
 
                     while (true)
                     {
-                        if (args.TryPop() is Form f)
-                        {
-                            f.Emit(vm, args);
-                        }
-                        else
-                        {
-                            break;
-                        }
+                        if (args.TryPop() is Form f) { f.Emit(vm, args); }
+                        else { break; }
                     }
 
                     foreach (var (fromIndex, toFrameOffset, toIndex) in brs)
@@ -556,45 +441,28 @@ public class Core : Lib
                     vm.Emit(Ops.EndFrame.Make());
                 });
             }
-            else
-            {
-                throw new EmitError(loc, "Missing bindings");
-            }
+            else { throw new EmitError(loc, "Missing bindings"); }
         });
 
         BindMacro("lib", [], (loc, target, vm, args) =>
         {
-            if (args.Count == 0)
-            {
-                vm.Emit(Ops.Push.Make(Value.Make(Core.Lib, vm.Lib)));
-            }
+            if (args.Count == 0) { vm.Emit(Ops.Push.Make(Value.Make(Core.Lib, vm.Lib))); }
+
             else if (args.TryPop() is Forms.Id nf)
             {
                 Lib? lib = null;
 
-                if (vm.Env.Find(nf.Name) is Value v)
-                {
-                    lib = v.Cast(loc, Core.Lib);
-                }
+                if (vm.Env.Find(nf.Name) is Value v) { lib = v.Cast(loc, Core.Lib); }
                 else
                 {
                     lib = new Lib(nf.Name, vm.Env, args.CollectIds());
                     vm.Env.BindLib(lib);
                 }
 
-                if (args.Empty)
-                {
-                    vm.Env = lib;
-                }
-                else
-                {
-                    vm.DoEnv(lib, () => args.Emit(vm, new Form.Queue()));
-                }
+                if (args.Empty) { vm.Env = lib; }
+                else { vm.DoEnv(lib, () => args.Emit(vm, new Form.Queue())); }
             }
-            else
-            {
-                throw new EmitError(loc, "Invalid library name");
-            }
+            else { throw new EmitError(loc, "Invalid library name"); }
         });
 
         BindMacro("load", ["path"], (loc, target, vm, args) =>
@@ -603,26 +471,14 @@ public class Core : Lib
             {
                 if (args.TryPop() is Form pf)
                 {
-                    if (vm.Eval(pf, args) is Value p)
-                    {
-                        vm.Load(p.Cast(pf.Loc, Core.String));
-                    }
-                    else
-                    {
-                        throw new EvalError(pf.Loc, "Missing path");
-                    }
+                    if (vm.Eval(pf, args) is Value p) { vm.Load(p.Cast(pf.Loc, Core.String)); }
+                    else { throw new EvalError(pf.Loc, "Missing path"); }
                 }
-                else
-                {
-                    break;
-                }
+                else { break; }
             }
         });
 
-        BindMethod("not", ["it"], (loc, target, vm, stack, arity) =>
-        {
-            stack.Push(Core.Bit, !(bool)stack.Pop());
-        });
+        BindMethod("not", ["it"], (loc, target, vm, stack, arity) => stack.Push(Core.Bit, !(bool)stack.Pop()));
 
         BindMacro("or", ["value1"], (loc, target, vm, args) =>
          {
@@ -631,11 +487,7 @@ public class Core : Lib
 
              while (!args.Empty)
              {
-                 if (!first)
-                 {
-                     vm.Emit(Ops.Drop.Make(1));
-                 }
-
+                 if (!first) { vm.Emit(Ops.Drop.Make(1)); }
                  args.Pop().Emit(vm, args);
 
                  if (!args.Empty)
@@ -669,14 +521,8 @@ public class Core : Lib
                     goto case 2;
             }
 
-            if (t is RangeTrait rt)
-            {
-                stack.Push(Core.Iter, rt.CreateRange(loc, min, max, stride));
-            }
-            else
-            {
-                throw new EvalError(loc, $"Invalid range type: {t}");
-            }
+            if (t is RangeTrait rt) { stack.Push(Iter, rt.CreateRange(loc, min, max, stride)); }
+            else { throw new EvalError(loc, $"Invalid range type: {t}"); }
         });
 
         BindMacro("return", [], (loc, target, vm, args) =>
@@ -686,14 +532,8 @@ public class Core : Lib
             if (args.TryPop() is Forms.Call c)
             {
 
-                if (c.Target is Forms.Literal lt && lt.Value is var lv && lv.Type == Core.UserMethod)
-                {
-                    m = lv.Cast(Core.UserMethod);
-                }
-                else if (c.Target is Forms.Id it && vm.Env[it.Name] is Value iv && iv.Type == Core.UserMethod)
-                {
-                    m = iv.Cast(Core.UserMethod);
-                }
+                if (c.Target is Forms.Literal lt && lt.Value is var lv && lv.Type == UserMethod) { m = lv.Cast(UserMethod); }
+                else if (c.Target is Forms.Id it && vm.Env[it.Name] is Value iv && iv.Type == UserMethod) { m = iv.Cast(UserMethod); }
 
                 if (m is UserMethod)
                 {
@@ -808,35 +648,21 @@ public class Core : Lib
             while (true)
             {
                 var id = args.TryPop();
-
-                if (id is null)
-                {
-                    break;
-                }
+                if (id is null) { break; }
 
                 if (id is Forms.Id idf)
                 {
                     if (args.TryPop() is Form f)
                     {
-                        if (f is Forms.Literal lit)
-                        {
-                            vm.Env[idf.Name] = lit.Value;
-                        }
-
+                        if (f is Forms.Literal lit) { vm.Env[idf.Name] = lit.Value; }
                         var v = new Form.Queue();
                         v.Push(f);
                         v.Emit(vm, args);
                         vm.Define(idf.Name);
                     }
-                    else
-                    {
-                        throw new EmitError(loc, "Missing value");
-                    }
+                    else { throw new EmitError(loc, "Missing value"); }
                 }
-                else
-                {
-                    throw new EmitError(loc, $"Invalid binding: {id}");
-                }
+                else { throw new EmitError(loc, $"Invalid binding: {id}"); }
             }
         });
     }
