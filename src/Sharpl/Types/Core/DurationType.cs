e2a28ac42f8555b3abbe12c80ddb972b9215b441
Andreas Wild <codr7@protonmail.com>
2024-09-13 23:04:29 +0200

    Implement duration numerics.

--- src/Sharpl/Types/Core/DurationType.cs
+++ src/Sharpl/Types/Core/DurationType.cs
@@ -10,55 +10,55 @@ public class DurationType(string name) :
 
     public void Add(Loc loc, VM vm, Stack stack, int arity)
     {
-        /*var res = 0;
+        var res = TimeSpan.FromTicks(0);
 
         while (arity > 0)
         {
-            res += stack.Pop().CastUnbox(loc, this);
+            res = res.Add(stack.Pop().CastUnbox(loc, this));
             arity--;
         }
 
-        stack.Push(this, res);*/
+        stack.Push(this, res);
     }
 
     public override bool Bool(Value value) => value.CastUnbox(this).CompareTo(ZERO)> 0;
 
     public void Divide(Loc loc, VM vm, Stack stack, int arity)
     {
-        /*stack.Reverse(arity);
+        stack.Reverse(arity);
         var res = stack.Pop().CastUnbox(loc, this);
         arity--;
 
         while (arity > 0)
         {
-            res /= stack.Pop().CastUnbox(loc, this);
+            res = res.Divide(stack.Pop().CastUnbox(loc, Libs.Core.Int));
             arity--;
         }
 
-        stack.Push(this, res);*/
+        stack.Push(this, res);
     }
 
     public void Multiply(Loc loc, VM vm, Stack stack, int arity)
     {
-        /*var res = stack.Pop().CastUnbox(loc, this);
+        var res = stack.Pop().CastUnbox(loc, this);
         arity--;
 
         while (arity > 0)
         {
-            res *= stack.Pop().CastUnbox(loc, this);
+            res = res.Multiply(stack.Pop().CastUnbox(loc, Libs.Core.Int));
             arity--;
         }
 
-        stack.Push(this, res);*/
+        stack.Push(this, res);
     }
 
     public void Subtract(Loc loc, VM vm, Stack stack, int arity)
     {
-        /*var res = 0;
+        var res = TimeSpan.FromTicks(0);
 
         if (arity > 0)
         {
-            if (arity == 1) { res = -stack.Pop().CastUnbox(loc, this); }
+            if (arity == 1) { res = res.Subtract(stack.Pop().CastUnbox(loc, this)); }
             else
             {
                 stack.Reverse(arity);
@@ -67,13 +67,13 @@ public class DurationType(string name) :
 
                 while (arity > 0)
                 {
-                    res -= stack.Pop().CastUnbox(loc, this);
+                    res = res.Subtract(stack.Pop().CastUnbox(loc, this));
                     arity--;
                 }
             }
         }
 
-        stack.Push(this, res);*/
+        stack.Push(this, res);
     }
 
     public override void Say(Value value, VM vm, StringBuilder result) => result.Append(value.CastUnbox(this));
