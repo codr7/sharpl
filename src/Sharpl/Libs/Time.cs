263d9f82e16c383b8d26ccac05e5f5250c7baf97
Andreas Wild <codr7@protonmail.com>
2024-09-13 22:47:23 +0200

    Add to/from-utc.

--- src/Sharpl/Libs/Time.cs
+++ src/Sharpl/Libs/Time.cs
@@ -4,10 +4,16 @@ public class Time : Lib
 {
     public Time() : base("time", null, [])
     {
+        BindMethod("from-utc", ["t"], (loc, target, vm, stack, arity) =>
+           stack.Push(Core.Timestamp, stack.Pop().CastUnbox(loc, Libs.Core.Timestamp).ToLocalTime()));
+
         BindMethod("now", [], (loc, target, vm, stack, arity) =>
-            stack.Push(Core.Timestamp, DateTime.Now));
+              stack.Push(Core.Timestamp, DateTime.Now));
 
         BindMethod("today", [], (loc, target, vm, stack, arity) =>
             stack.Push(Core.Timestamp, DateTime.Now.Date));
+
+        BindMethod("to-utc", ["t"], (loc, target, vm, stack, arity) =>
+           stack.Push(Core.Timestamp, stack.Pop().CastUnbox(loc, Libs.Core.Timestamp).ToUniversalTime()));
     }
 }
