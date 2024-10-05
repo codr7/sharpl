1ce71ff3fe76cf7627342d9736911e7325cf244e
Andreas Nilsson <codr7@protonmail.com>
2024-10-06 00:02:58 +0200

    Todo.

--- src/Sharpl/Types/Core/ErrorType.cs
+++ src/Sharpl/Types/Core/ErrorType.cs
@@ -2,7 +2,7 @@
 
 namespace Sharpl.Types.Core;
 
-public class ErrorType(string name, AnyType[] parents) : Type<UserError>(name, parents) {
+public class ErrorType(string name, AnyType[] parents) : Type<EvalError>(name, parents) {
     public override void Call(VM vm, Stack stack, int arity, Loc loc)
     {
         var res = new StringBuilder();
